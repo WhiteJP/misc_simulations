e9ffff331b7d04f39d427ec917551741383cc958
Joshua White <jpwhite@mit.edu>
2024-03-28 18:33:20 -0400

    annotate code

--- monty_hall_sims.R
+++ monty_hall_sims.R
@@ -1,22 +1,27 @@
-## Monty Hall simluation
+## Monty Hall simulation
 
-nsims <- 100000
+# setup
+nsims <- 1e5
 outcomes <- c("goat", "goat", "car")
-stay_result <- switch_result <- logical(nsims)
+stay_result <- switch_result <- logical(nsims) 
+
 for (i in seq(nsims)) {
-  doors <- sample(outcomes, 3)
-  choice <- sample.int(3, 1)
+  doors <- sample(outcomes, 3) # shuffle outcomes behind doors
+  choice <- sample.int(3, 1) # Player chooses a door
   
-  goats_left <- setdiff(which(doors == "goat"), choice)
+  # Monty opens a door with a goat
+  goats_left <- setdiff(which(doors == "goat"), choice) #index of goats left
   if(length(goats_left) == 1) {
-    doors[goats_left] <- NA
+    doors[goats_left] <- NA # if one goat left open that door
   } else {
-    doors[sample(goats_left, 1)] <- NA
+    doors[sample(goats_left, 1)] <- NA # if two goats randomly open one
   }
   
-  stay_result[i] <- doors[choice] == "car"
+  # result
+  stay_result[i] <- doors[choice] == "car" 
   switch_result[i] <- doors[!is.na(doors) & seq_along(doors) != choice] == "car"
 }
 
+# results
 mean(stay_result)
 mean(switch_result)
