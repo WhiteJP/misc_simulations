b1f8f2f0dfa497ec8e61ef9dceb8740ec8274e22
Joshua White <jpwhite@mit.edu>
2025-01-30 14:48:37 -0500

    Create Lin-estimator-v-no-interaction.R

---
+++ Lin-estimator-v-no-interaction.R
@@ -0,0 +1,29 @@
+## Lin estimator, with v without interaction
+
+pre <- rnorm(1000, 25, 7.5)
+treat <- sample(c("control", "treatment"), 1000, replace = TRUE)
+
+# add a treatment effect that depends on pre score for treatment group
+post <- ifelse(treat == "control", pre, pre + pre*0.8) + rnorm(1000, 0, 5)
+change <- post - pre
+
+#zscore pre
+zpre <- (pre - mean(pre))/sd(pre)
+
+#plot
+library(ggplot2)
+d <- data.frame(zpre = zpre, post = post, change = change, treat = treat)
+d$treat <- factor(d$treat, levels = c("control", "treatment"))
+
+ggplot(d, aes(x = zpre, y = change, color = treat)) + 
+  geom_point() + 
+  geom_smooth(method = "lm", se = FALSE) 
+
+ggplot(d, aes(x = zpre, y = change, color = treat)) + 
+  geom_point() + 
+  moderndive::geom_parallel_slopes()
+
+#models
+lm(change ~ zpre + treat, data = d) |> summary()
+lm(change ~ zpre*treat, data = d) |> summary()
+              
